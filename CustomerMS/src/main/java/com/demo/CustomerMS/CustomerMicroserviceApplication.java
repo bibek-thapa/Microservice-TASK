7e4dbcb686ac1431dfa45b27da7d09262c1ee37f
bibek-thapa <bibek.thaphere1@gmail.com>
2020-06-01 21:17:02 -0500

    Added the config server and eureka registry

--- CustomerMS/src/main/java/com/demo/CustomerMS/CustomerMicroserviceApplication.java
+++ CustomerMS/src/main/java/com/demo/CustomerMS/CustomerMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.demo.CustomerMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class CustomerMicroserviceApplication {
--- EquipmentMS/src/main/java/com/demo/EquipmentMS/EquipmentMsApplication.java
+++ EquipmentMS/src/main/java/com/demo/EquipmentMS/EquipmentMsApplication.java
@@ -2,7 +2,9 @@ package com.demo.EquipmentMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EquipmentMsApplication {
 
