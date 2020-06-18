cd30a60d79b1973e2e008574370e66f723cc0bda
Bibek Thapa <bek@Bibeks-MBP.attlocal.net>
2020-06-17 22:42:01 -0500

    Added the swagger configuration

--- CustomerMS/src/main/java/com/demo/CustomerMS/CustomerMicroserviceApplication.java
+++ CustomerMS/src/main/java/com/demo/CustomerMS/CustomerMicroserviceApplication.java
@@ -7,11 +7,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
+@EnableSwagger2
+
 public class CustomerMicroserviceApplication {
 
 	public static void main(String[] args) {
--- ProfileService/src/main/java/com/demo/ProfileService/ProfileServiceApplication.java
+++ ProfileService/src/main/java/com/demo/ProfileService/ProfileServiceApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
-@ComponentScan(basePackages="com.demo.*")
+@EnableSwagger2
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
